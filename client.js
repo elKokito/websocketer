d28456386480309af7f44dc55c8621fd257db6bc
not me <laskdf>
2016-05-15 20:09:03 -0400

    added socket route for connection

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 var config = require('./.config.json');
-console.log('connecting to -> ', config.remote_server);
-var s = require('socket.io-client')(config.remote_server);
+console.log('connecting to -> ', config.remote_websocket);
+var s = require('socket.io-client')(config.remote_websocket);
 var request = require('superagent');
 
 s.on('connect', () => {
@@ -8,6 +8,7 @@ s.on('connect', () => {
 });
 
 s.on('all_torrents', (data) => {
+    console.log('on all_torrents');
     request
         .get(config.local_server + 'all_torrents')
         .end((err, res) => {
@@ -17,6 +18,7 @@ s.on('all_torrents', (data) => {
 });
 
 s.on('movies', function(data) {
+    console.log('on movies');
     request
         .get(config.local_server + 'movies')
         .end((err, res) => {
@@ -26,6 +28,7 @@ s.on('movies', function(data) {
 });
 
 s.on('series', function(data) {
+    console.log('on series');
     request
         .get(config.local_server + 'series')
         .end((err, res) => {
@@ -35,6 +38,7 @@ s.on('series', function(data) {
 });
 
 s.on('add_torrent', function(data) {
+    console.log('on add_torrent');
     console.log('add_torrent ->', data);
     request
         .post(config.local_server + 'add_torrent')
